e342acbee9977cd1f8dad248d6cdf3eb5a18167d
yapeng hao <yapeng.hao@email.com>
2019-09-25 16:18:45 -0400

    test

--- ConsoleApp5-0925-2/ConsoleApp5-0925-2/Program.cs
+++ ConsoleApp5-0925-2/ConsoleApp5-0925-2/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp5_0925_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("master");
+            Console.WriteLine("DEV");
         }
     }
 }
